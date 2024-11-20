33bbdfddd93116e5acf5c6236136e5f1c85ec72c
Liam <40646739@live.napier.ac.uk>
2024-11-20 15:55:49 +0000

    updated sql connect and disconnect methods

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -1,13 +1,14 @@
 package com.napier.sem;
 
 import java.sql.*;
+import java.util.concurrent.TimeUnit;
 
 public class App {
 
     /**
      * Connection to MySQL database.
      */
-    private Connection con = null;
+    public Connection con;
 
     /**
      * Connect to the MySQL database.
@@ -21,25 +22,56 @@ public class App {
             System.exit(-1);
         }
 
-        int retries = 10;
+        int retries = 100;
         for (int i = 0; i < retries; ++i) {
             System.out.println("Connecting to database...");
             try {
                 // Wait a bit for db to start
-                Thread.sleep(30000);
+                Thread.sleep(3000);
                 // Connect to database
-                con = DriverManager.getConnection("jdbc:mysql://db:3306/employees?useSSL=false", "root", "example");
+                con = DriverManager.getConnection("jdbc:mysql://db:3306/employees?allowPublicKeyRetrieval=true&useSSL=false", "root", "example");
                 System.out.println("Successfully connected");
                 break;
             } catch (SQLException sqle) {
-                System.out.println("Failed to connect to database attempt " + Integer.toString(i));
+                System.out.println("Failed to connect to database attempt " + i);
                 System.out.println(sqle.getMessage());
             } catch (InterruptedException ie) {
                 System.out.println("Thread interrupted? Should not happen.");
             }
         }
     }
-
+    public Employee getEmployee(int ID)
+    {
+        try
+        {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+            // Create string for SQL statement
+            String strSelect =
+                              "SELECT * "
+                            + "FROM employees ";
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+            // Return new employee if valid.
+            // Check one is returned
+            if (rset.next())
+            {
+                Employee emp = new Employee();
+                emp.emp_no = rset.getInt("emp_no");
+                emp.first_name = rset.getString("first_name");
+                emp.last_name = rset.getString("last_name");
+                return emp;
+            }
+            else
+                return null;
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get employee details");
+            return null;
+        }
+    }
     /**
      * Disconnect from the MySQL database.
      */
@@ -54,14 +86,36 @@ public class App {
         }
     }
 
-    public static void main(String[] args) {
+
+
+    public void displayEmployee(Employee emp)
+    {
+        if (emp != null)
+        {
+            System.out.println(
+                    emp.emp_no + " "
+                            + emp.first_name + " "
+                            + emp.last_name + "\n"
+                            + emp.title + "\n"
+                            + "Salary:" + emp.salary + "\n"
+                            + emp.dept_name + "\n"
+                            + "Manager: " + emp.manager + "\n");
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
         // Create new Application
         App a = new App();
 
         // Connect to database
         a.connect();
-
+        TimeUnit.SECONDS.sleep(10);
+        //get employee data
+        Employee emp = a.getEmployee(255530);
+        a.displayEmployee(emp);
         // Disconnect from database
         a.disconnect();
+
+
     }
 }
